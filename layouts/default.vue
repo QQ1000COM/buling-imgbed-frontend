dee135687f1ac4ceefa4dea77f5348db2f9ebec2
QQ1000COM <102141276+QQ1000COM@users.noreply.github.com>
2025-01-18 14:04:31 +0800

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,8 +3,8 @@
     <nav class="navbar">
       <div class="container nav-container">
         <NuxtLink to="/" class="logo">
-          <span class="logo-full">布灵图床</span>
-          <span class="logo-short">布灵</span>
+          <span class="logo-full">QQ1000图床</span>
+          <span class="logo-short">QQ1000</span>
         </NuxtLink>
 
         <div class="nav-links">
@@ -30,11 +30,10 @@
 
     <footer class="footer">
       <div class="container footer-content">
-        <p class="copyright">© {{ currentYear }} 布灵图床 · 简单好用的图床服务 </p>
-        <p class="copyright">❤️ Made with <a href="https://anuuu.com" target="_blank" rel="noopener"
+        <p class="copyright">© {{ currentYear }} 全球加速 · 简单好用的图床服务 </p>
+        <p class="copyright">❤️ Made with <a href="https://www.qq1000.com" target="_blank" rel="noopener"
             class="footer-link">Anuuu.com</a> ·
           <a href="https://github.com/wzs8/buling-imgbed" target="_blank" rel="noopener" class="footer-link">GitHub</a> ·
-          <a href="https://anuuu.com/buling-imgbed.html" rel="noopener" class="footer-link" target="_blank">使用教程</a>
         </p>
       </div>
     </footer>
